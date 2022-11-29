8b8f1cb5436c0608b50bf1832fe425d26a5845fd
Yu950831 <s212610@dtu.dk>
2022-11-29 11:36:50 +0100

    new solutions
    make all the data to clustering but not only one protein

---
+++ operations/day1_all_pca.R
@@ -0,0 +1,5 @@
+### we found it is meaningless to compare the same proteins in different groups
+### so we decide to make the different groups of the protein into one group and 
+### use them to do the clustering.
+
+### load the data
